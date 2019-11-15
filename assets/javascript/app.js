909309b427d178f8e53c46f269f975a5d6830423
Cassondra Reinsel <cassiereinsel@gmail.com>
2019-11-15 12:03:54 -0800

    basic structure, still working on input and alcohol click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-
+var count=0;
+var mood;
 //to hide divs when page is first accessed
 function hide(){
     $("#of-age").hide();
@@ -9,10 +10,93 @@ function hide(){
 
 hide();
 
-// goes to 21+ questioon after get strated is clicked
+// goes to 21+ question after get started is clicked
 $("#click-me").on("click", function(event){
 
     $("#welcome").hide();
     $("#of-age").show();
     
+});
+
+$("#yes").on("click", function(event){
+    count++
+    $("#of-age").hide();
+    $("#movie-selection").show();
+});
+
+
+$("#no").on("click", function(event){
+    $("#of-age").hide();
+    $("#movie-selection").show();
+})
+
+$(".mood-one").on("click", function(event){
+    $("#movie-selection").hide();
+    $("#drink-selection").show();
+    if (count===0){
+        $(".alcohol").hide();
+        $(".hide").hide();
+    }
+    
+    //mood=whatever the mood is 
+});
+
+$(".mood-two").on("click", function(event){
+    $("#movie-selection").hide();
+    $("#drink-selection").show();
+    if (count===0){
+        $(".alcohol").hide();
+        $(".hide").hide();
+    }
+    //mood=whatever the mood is
+});
+
+$(".mood-three").on("click", function(event){
+    $("#movie-selection").hide();
+    $("#drink-selection").show();
+    if (count===0){
+        $(".alcohol").hide();
+        $(".hide").hide();
+    }
+    //mood=whatever the mood is
+});
+
+$(".mood-four").on("click", function(event){
+    $("#movie-selection").hide();
+    $("#drink-selection").show();
+    if (count===0){
+        $(".alcohol").hide();
+        $(".hide").hide();
+    }
+    //mood=whatever the mood is
+});
+
+$(".mood-five").on("click", function(event){
+    $("#movie-selection").hide();
+    $("#drink-selection").show();
+    if (count===0){
+        $(".alcohol").hide();
+        $(".hide").hide();
+    }
+    //mood=whatever the mood is
+});
+
+//turns user input into value for api search later
+alcoholType=$(".alcohol").val().trim();
+
+$(".no-alcohol").on("click", function(event){
+    $("#drink-selection").hide();
+    $("#choices").show();
+    //pick random non-alcoholic drinks for choices div via api
+});
+
+$(".alcohol").on("click", function(event){
+    // if(alcoholType){
+    //     Swal.fire('Please Select an Alcohol Type');
+    // }
+    console.log(alcoholType);
+    $("#drink-selection").hide();
+    $("#choices").show();
+    
+    //pick random alcoholic drinks for choices div via api with alcoholType var
 });
